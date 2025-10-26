587b0388806c4159d2b7d0e7297babcbc55c84c0
Hell <filip.petkov.99@gmail.com>
2025-10-26 16:17:58 +0200

    Fixed Mapper

--- Employees.Web/Dtos/Mapper.cs
+++ Employees.Web/Dtos/Mapper.cs
@@ -7,7 +7,7 @@ namespace Employees.Web.Dtos
     {
         public static ProjectDto MapToDto(this Project project)
         {
-            return new ProjectDto(project.Id, project.EmployeeId, project.Name, project.From, project.To);
+            return new ProjectDto(project.Id, project.EmployeeId, project.Name, project.From, project.To == null ? DateTime.Now : project.To);
         }
     }
 }
